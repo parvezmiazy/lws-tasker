38ebf222d4034015f7dfca58ec960f20597a2218
Parvez Ahmed <miazyparvez@gmail.com>
2024-01-16 22:17:49 +0600

    Initial project setup

---
+++ src/tasks/TaskBoard.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import SearchTask from "./SearchTask";
+import TaskActions from "./TaskActions";
+import TaskList from "./TaskList";
+
+export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React Native",
+    description: "I want to learn react from learn with sumit website",
+    tags: ["web", "React", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTask]);
+  return (
+    <section className="mb-20" id="tasks">
+      <div className="container">
+        <div className="p-2 flex justify-end">
+          <SearchTask />
+        </div>
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+          <TaskActions />
+          <TaskList tasks={tasks} />
+        </div>
+      </div>
+    </section>
+  );
+}
